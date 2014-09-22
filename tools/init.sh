43b12ac4c82249a104871e98262f95fbce3a2926
Tatsunori TAMAKI <github@qtamaki.com>
2014-09-22 17:54:12 +0900

    remove tmp dir.

--- tools/init.sh
+++ tools/init.sh
@@ -9,6 +9,7 @@ if [[ $? -ne 9 ]];then
   useradd -d / -u $OWNER_UID -p '$6$eR60h2j0iFI/6$Rydt2yT0gZaIXpZENV53rAqwDzn.jYYAjQNes33ALBMzxyOwg04AkK7PJAiAhFoitfu6.WVeBTxhU2zvbvKBt0' -g owner -G sudo owner
 fi
 cd /work/three-d-mind
+rm -rf tmp
 bundle install
 export RAILS_ENV=production
 export SECRET_KEY_BASE=`rake secret`
